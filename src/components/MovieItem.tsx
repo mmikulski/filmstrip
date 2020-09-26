959c575a3706a3a0d400416687de418301547fbb
Maciej Mikulski <maciej.mikulski@sparklab.pl>
2020-09-26 11:00:22 +0200

    Initial MovieItem styles.

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,21 +1,74 @@
 import React from "react";
 import {MovieResult} from "./MoviesList";
+import styled from "styled-components";
 
 export function MovieItem({movie}: { movie: MovieResult }) {
   return (
     <li key={movie.imdbID}>
-      <div>
-        <div>
-          <img src={movie.Poster} alt={movie.Title}/>
-        </div>
-        <h1>{movie.Title} <span>({movie.Year})</span></h1>
-      </div>
+      <Frame>
+        <Content>
+          <PosterContainer>
+            <img src={movie.Poster} alt={movie.Title}/>
+          </PosterContainer>
+          <Title>{movie.Title} <span>({movie.Year})</span></Title>
+        </Content>
+      </Frame>
     </li>
   );
 }
 
 export function MovieItemPlaceholder() {
   return (
-    <li>Loading</li>
+    <li>
+      <Frame>
+        <Content>
+          <Title>Loading</Title>
+        </Content>
+      </Frame>
+    </li>
   )
 }
+
+const Frame = styled.div`
+  & {
+    border-top: #111111 1px solid;
+    display: flex;
+  }
+  &:last-child {
+    border-bottom: #111111 1px solid;
+  }
+  
+  &:before {
+    left: -10px;
+  }
+  
+  &:after {
+    left: 10px;
+  }
+  
+  &:before, &:after {
+    width: 10px;
+    display: block;
+    height: 95px;
+    content: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQYV2NgAAIAAAUAAarVyFEAAAAASUVORK5CYII=   );
+    position: relative;
+    top: -1px;
+    bottom: 0;
+    right: 0;
+    background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAALUlEQVQoU2NkYGD4z0AEYCRHIUgTNgC2EWYiLkUwjf8HgUKYewl6hmBIEh2OAAs1EAGU8ZmkAAAAAElFTkSuQmCC);
+    background-repeat: repeat;
+  }
+`
+
+const Content = styled.div`
+  width: 100%;
+`
+
+const PosterContainer = styled.div`
+  padding: 10px;
+`
+
+const Title = styled.h1`
+  color: #000;
+  font-size: 1vw;
+`
--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {MovieItem, MovieItemPlaceholder} from "./MovieItem";
+import styled from "styled-components";
 
 export type MovieResult = {
   Title: string,
@@ -11,16 +12,20 @@ export type MovieResult = {
 
 export const MOVIE_LIST_CONTAINER_TESTID = 'movieListContainerTestid';
 
-export function MoviesList({movies}: {movies: MovieResult[]}) {
+export function MoviesList({movies}: { movies: MovieResult[] }) {
   return (
-    <div data-testid={MOVIE_LIST_CONTAINER_TESTID}>
+    <ListContainer data-testid={MOVIE_LIST_CONTAINER_TESTID}>
       <ul>
         {(movies.length > 0) ? (
-          movies.map((movie: MovieResult) => (<MovieItem movie={movie} />))
+          movies.map((movie: MovieResult) => (<MovieItem movie={movie}/>))
         ) : (
-          <MovieItemPlaceholder />
+          <MovieItemPlaceholder/>
         )}
       </ul>
-    </div>
+    </ListContainer>
   );
 }
+
+const ListContainer = styled.div`
+  width: 720px;
+`
